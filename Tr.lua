d75764222d328a00eefbd67b0b285f8798809ead
Souxch06 <owenafonso410@gmail.com>
2025-10-29 14:54:18 +0100

    Update Tr.lua

--- Tr.lua
+++ Tr.lua
@@ -1,1 +1,5 @@
-print("✅ Script GitHub chargé avec succès !") 
+game.StarterGui:SetCore("SendNotification", {
+    Title = "✅ Script chargé",
+    Text = "Ton script GitHub fonctionne !",
+    Duration = 5
+})
